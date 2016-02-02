a67224f615d0b2cda6621a2ae5a49438913bffaf
FezGirl <fi.grasshopper@gmail.com>
2016-02-01 17:14:09 -0800

    Initilized one talon and double solenoid. Wrote a set talon method. Code needs to be written for the solenoid.

--- Stronghold/src/org/usfirst/frc/team4180/robot/Shooter.java
+++ Stronghold/src/org/usfirst/frc/team4180/robot/Shooter.java
@@ -1,9 +1,29 @@
 package org.usfirst.frc.team4180.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.TalonSRX;
+
 public class Shooter {
+//  Notes from Hardware/Design:	
+//	there will be one pneumatic piston which moves in and out
+//	there will be one motor (Talon SRX) which will turn in both directions
+//	motor will need to be controlled by a button
+	public static final int SHOOTER_TALON_PORT = 0; //place holder value
+	public static final int SHOOTER_SOLENOID_1 = 0;  //place holder value
+	public static final int SHOOTER_SOLENOID_2 = 0;  //place holder value
+	
+	private TalonSRX shooterTalon;
+	private DoubleSolenoid shooterSolenoid;
 	
 	public Shooter() {
-	
+		shooterTalon = new TalonSRX(SHOOTER_TALON_PORT);
+		shooterSolenoid = new DoubleSolenoid(SHOOTER_SOLENOID_1, SHOOTER_SOLENOID_2);
 	}
+	
+	public void setShooterTalon(double shooterTalonSpeed){
+		shooterTalon.set(shooterTalonSpeed);
+	}
+	
+	//Set Pneumatics???
 
 }
