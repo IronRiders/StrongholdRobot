f746140382f840bbd8062f59003753c22debe1b9
Clyde Westrom <kiwiwiiwii@gmail.com>
2016-02-02 16:08:36 -0800

    Rewrote the solenoid method. Brendan removed commented code.

--- Stronghold/src/org/usfirst/frc/team4180/robot/Shooter.java
+++ Stronghold/src/org/usfirst/frc/team4180/robot/Shooter.java
@@ -9,8 +9,8 @@ public class Shooter {
 //	there will be one pneumatic piston which moves in and out (single solenoid)
 //	there will be one motor (Talon SRX) which will turn in both directions
 //	motor will need to be controlled by a button
-	public static final int SHOOTER_TALON_PORT = 0; //place holder value
-	public static final int SHOOTER_SOLENOID = 0;  //place holder value
+	private static final int SHOOTER_TALON_PORT = 0; //place holder value
+	private static final int SHOOTER_SOLENOID = 0;  //place holder value
 	
 	private TalonSRX shooterTalon;
 	private Solenoid shooterSolenoid;
@@ -25,29 +25,9 @@ public class Shooter {
 	}
 	
 	//turns on the solenoid
-	public void solenoidOn() {
-		shooterSolenoid.set(true);
+	public void setSolenoid(boolean state) {
+		shooterSolenoid.set(state);
 	}
 	
-	//turns off the solenoid
-	public void solenoidOff() {
-		shooterSolenoid.set(false);
-	}
-	
-// Here just in case we need to change back to double solenoids
-//	//Turns the solenoid on forwards
-//	public void pistonSolenoidForward() {
-//		shooterSolenoid.set(DoubleSolenoid.Value.kForward);
-//	}
-//	
-//	//Turns both solenoids off
-//	//maybe unnecessary
-//	public void pistonSolenoidOff() {
-//		shooterSolenoid.set(DoubleSolenoid.Value.kOff);
-//	}
-//	
-//	//Turns the solenoid on backwards
-//	public void pistonSolenoidReverse() {
-//		shooterSolenoid.set(DoubleSolenoid.Value.kReverse);
-//	}
+//We may be switching to double solenoids later
 }
